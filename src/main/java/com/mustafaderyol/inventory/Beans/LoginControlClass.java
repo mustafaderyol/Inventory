aebde19294489c8b85fcea64bcede649e711cbba
Mustafa DERYOL <mustafa.deryol@hotmail.com>
2015-12-09 19:32:53 +0200

    Spring Security with Login
    Spring Security with Login

---
+++ src/main/java/com/mustafaderyol/inventory/Beans/LoginControlClass.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mustafaderyol.inventory.beans;
+
+import java.io.IOException;
+import javax.faces.application.NavigationHandler;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+/**
+ *
+ * @author MstfDryl
+ */
+@ManagedBean
+@SessionScoped
+public class LoginControlClass {
+
+   public String doLogin(ActionEvent event) throws ServletException, IOException {
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+
+        RequestDispatcher dispatcher = ((ServletRequest) context.getRequest())
+                .getRequestDispatcher("/j_spring_security_check");
+
+        dispatcher.forward((ServletRequest) context.getRequest(),
+                (ServletResponse) context.getResponse());
+
+        FacesContext.getCurrentInstance().responseComplete();
+
+        return null;
+    }
+
+
+    public void authorizedUserControl(){
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(!authentication.getPrincipal().toString().equals("anonymousUser")){
+
+            NavigationHandler nh =  FacesContext.getCurrentInstance().getApplication().getNavigationHandler();
+            nh.handleNavigation(FacesContext.getCurrentInstance(), null, "/dashboard.xhtml?faces-redirect=true");
+
+        }
+    }
+    
+}
---
+++ src/main/java/com/mustafaderyol/inventory/Entity/PersonalRoles.java
@@ -0,0 +1,57 @@
+package com.mustafaderyol.inventory.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+/**
+ *
+ * @author MstfDryl
+ */
+@Entity(name="PERSONALROLES")
+@NamedQueries({
+    @NamedQuery(name="allPersonalRoles",query="SELECT a FROM PERSONALROLES a")
+})
+public class PersonalRoles {
+    private static final long serialVersionUID = 1L;
+    
+    @Column(name="ID")
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    
+    @Column(name="PERSONALID",nullable=false,length=150)
+    private Long personalid;
+    
+    @Column(name="NAME",nullable=false,length=150)
+    private String name;
+
+    public PersonalRoles() {
+    }
+
+    public PersonalRoles(Long personalid, String name) {
+        this.personalid = personalid;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPersonalid() {
+        return personalid;
+    }
+
+    public void setPersonalid(Long personalid) {
+        this.personalid = personalid;
+    }
+   
+}
